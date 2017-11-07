d69b39aff3bb3def9a2cb240ca0b5e21b3e87df8
Matthieu Boileau <matthieu.boileau@math.unistra.fr>
2017-11-07 08:39:18 +0100

    A first conversion to CoursPython

--- scripts/make_slides.sh
+++ scripts/make_slides.sh
@@ -6,29 +6,23 @@ if [ "$1" != "--url" ]
 then
   # Useful for running in local with internet connexion
   revealprefix="reveal.js"
+  cp -r reveal.js public/
 else
   # Needed for online publication by gitlab pages
   revealprefix="https://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.3.0"
 fi
 
-notebooks="J1/00-InitPython-generalites.ipynb \
-           J1/01-InitPython-langage.ipynb \
-           J2/02-InitPython-langage.ipynb \
-           J2/03-InitPython-microprojet.ipynb \
-           J3/04-InitPython-langage.ipynb" 
+notebooks="01-CoursPython-generalites/01-CoursPython-generalites.ipynb \
+           02-CoursPython-langage/02-CoursPython-langage.ipynb"
 
 jupyter-nbconvert $notebooks --to slides --reveal-prefix $revealprefix --output-dir=public
 
 jupyter-nbconvert index.ipynb --to html --output-dir=public
 
-cp J*/*-InitPython-*.ipynb public  # uploaded notebooks will be rendered by nbviewer
+cp *-CoursPython-*/*-CoursPython-*.ipynb public  # uploaded notebooks will be rendered by nbviewer
 
-if [ "$1" != "--url" ]
-then
-  cp -r reveal.js public/
-fi
-
-cp -r J1/fig public
-cp J2/fig/* public/fig
+mkdir public/fig
+cp 01-CoursPython-generalites/fig/* public/fig
+cp 02-CoursPython-langage/fig/* public/fig
 
 
