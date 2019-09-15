464ed520fba9e11b78d4283276128d2477608655
Matthieu Boileau <matthieu.boileau@math.unistra.fr>
2019-09-15 19:01:20 +0200

    homepage: select working chapter by env variable

--- homepage/generate_homepage.py
+++ homepage/generate_homepage.py
@@ -12,15 +12,29 @@ script_path = os.path.relpath(os.path.join(os.path.dirname(__file__)))
 VARIABLES_FILE = os.path.join(script_path, "variables.yml")
 HTML_TEMPLATE = "template.html"
 print(HTML_TEMPLATE)
+ENV_VARIABLE_PREFIX = 'COURSE'
+
 
 def get_variables(variables_file=VARIABLES_FILE):
     """Parse yaml file and return the corresponding configuration dictionary"""
     with open(variables_file, 'r') as f:
         variables = yaml.safe_load(f)
-    # Update config with environment variable (type COURSE_GITLAB_URL) if exists
+    # Update config with environment variable (type COURSE_GITLAB_URL)
+    # if exists
     for variable in variables:
-        environment_variable = 'COURSE_{}'.format(variable.upper())
-        variables[variable] = os.environ.get(environment_variable, variables[variable])
+        environment_variable = '{}_{}'.format(ENV_VARIABLE_PREFIX,
+                                              variable.upper())
+        variables[variable] = os.environ.get(environment_variable,
+                                             variables[variable])
+    # Make ichapter > working_chapter unavailable for download
+    working_chapter = int(os.environ.get('{}_WORKING_CHAPTER'.format(
+                                         ENV_VARIABLE_PREFIX)))
+    if working_chapter:
+        for ichapter, chapter in enumerate(variables['chapters']):
+            print(ichapter, chapter)
+            if ichapter >= working_chapter:
+                variables['chapters'][ichapter]['preview_only'] = True
+
     return variables
 
 
